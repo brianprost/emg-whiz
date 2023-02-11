5335c7c8e5e0b5865984ef3d95a1dc523c0c2f96
Brian Prost <brian.s.prost@gmail.com>
2023-02-11 16:27:23 +0000

    new format for processing each transformation
    this way, we can more easily do each transformation and prioritize the non-destructive transformations first

--- data/transform_data.py
+++ data/transform_data.py
@@ -10,11 +10,13 @@ file_transformations = [
         'file_to_transform': 'cases abnormal muscles.xlsx',
         'column_transformations': [
             {
-                'isDelete': False,
-                'file_with_name': 'muscles main.xlsx',
-                'column_to_transform': 'Name',
-                'column_with_name': 'Name',
-                'primary_key': 'ID',
+                'simple_transformation': {
+                    'isDelete': False,
+                    'file_with_name': 'muscles main.xlsx',
+                    'column_to_transform': 'Name',
+                    'column_with_name': 'Name',
+                    'primary_key': 'ID',
+                }
             }
         ],
         'export_file_name': 'cases abnormal muscles transformed.xlsx',
@@ -23,11 +25,13 @@ file_transformations = [
         'file_to_transform': 'cases abnormal nerves.xlsx',
         'column_transformations': [
             {
-                'isDelete': False,
-                'file_with_name': 'nerves main.xlsx',
-                'column_to_transform': 'Name',
-                'column_with_name': 'Name',
-                'primary_key': 'ID',
+                'simple_transformation': {
+                    'isDelete': False,
+                    'file_with_name': 'nerves main.xlsx',
+                    'column_to_transform': 'Name',
+                    'column_with_name': 'Name',
+                    'primary_key': 'ID',
+                }
             }
         ],
         'export_file_name': 'cases abnormal nerves transformed.xlsx',
@@ -36,31 +40,34 @@ file_transformations = [
         'file_to_transform': 'cases diagnosis.xlsx',
         'column_transformations': [
             {
-                'isDelete': False,
-                'file_with_name': 'diagnoses names (to destroy).xlsx',
-                'column_to_transform': 'Diagnosis',
-                'column_with_name': 'diag_name',
-                'primary_key': 'diag_name_id',
+                'simple_transformation': {
+                    'isDelete': False,
+                    'file_with_name': 'diagnoses names (to destroy).xlsx',
+                    'column_to_transform': 'Diagnosis',
+                    'column_with_name': 'diag_name',
+                    'primary_key': 'diag_name_id',
+                }
             },
             {
-                'isDelete': False,
-                'isSpecial': True,
-                'specialTransform': [
-                    'cases_diagnosis_ncs_criteria_transformation',
-                    'cases_diagnosis_emg_criteria_transformation',
-                ],
-            }
+                'cases_diagnosis_ncs_criteria_transformation'
+            },
+            {
+                'cases_diagnosis_emg_criteria_transformation'
+            },
         ],
     },
     {
         'file_to_transform': 'cc relations.xlsx',
         'column_transformations': [
             {
-                'isDelete': False,
-                'file_with_name': 'cc names.xlsx',
-                'column_to_transform': 'item_id',
-                'column_with_name': 'item_name',
-                'primary_key': 'item_id',
+                'simple_transformation':
+                {
+                    'isDelete': False,
+                    'file_with_name': 'cc names.xlsx',
+                    'column_to_transform': 'item_id',
+                    'column_with_name': 'item_name',
+                    'primary_key': 'item_id',
+                }
             }
         ],
         'export_file_name': 'cc relations transformed.xlsx',
@@ -69,16 +76,14 @@ file_transformations = [
         'file_to_transform': 'cases main.xlsx',
         'column_transformations': [
             {
-                'isDelete': True,
-                'column_to_transform': 'case_num',
+                'simple_transformation': {
+                    'isDelete': True,
+                    'column_to_transform': 'case_num',
+                },
             },
             {
-                'isDelete': False,
-                'isSpecial': True,
-                'specialTransform': [
-                    'cases_main_cc_transformation',
-                ],
-            }
+                'cases_main_cc_transformation',
+            },
         ],
         'export_file_name': 'cases main transformed.xlsx',
     },
@@ -86,8 +91,10 @@ file_transformations = [
         'file_to_transform': 'modules main.xlsx',
         'column_transformations': [
             {
-                'isDelete': True,
-                'column_to_transform': 'ID',
+                'simple_transformation': {
+                    'isDelete': True,
+                    'column_to_transform': 'ID',
+                },
             },
         ],
     }
@@ -96,38 +103,11 @@ file_transformations = [
 
 def main():
     for file_transformation in file_transformations:
-        # Read in the file_to_transform file
-        to_transform_df = pd.read_excel(
-            f'original/{file_transformation["file_to_transform"]}')
-
-        # Loop through the column transformations
-        for column_transformation in file_transformation['column_transformations']:
-            print(
-                f'Transforming {column_transformation["column_to_transform"]} in {file_transformation["file_to_transform"]}')
-            # If the column is to be deleted, delete it and continue to the next column transformation
-            if column_transformation['isDelete']:
-                print(
-                    f'Deleting {column_transformation["column_to_transform"]}')
-                del to_transform_df[column_transformation['column_to_transform']]
-                continue
-
-            # Read in the file_with_name file
-            with_name_df = pd.read_excel(
-                f'original/{column_transformation["file_with_name"]}')
-
-            # Create a dictionary to map ID to nerve/muscle names
-            name_dict = with_name_df.set_index(
-                column_transformation['primary_key'])[column_transformation['column_with_name']].to_dict()
-
-            # Use the map function to replace the ID in the file_to_transform file with the corresponding nerve/muscle name
-            to_transform_df[column_transformation['column_to_transform']
-                            ] = to_transform_df[column_transformation['column_to_transform']].map(name_dict)
-        # Save the modified file_to_transformed file
-        to_transform_df.to_excel(
-            f'transformed/{file_transformation["export_file_name"]}', index=False)
+        pass
 
     transformations_dict = {
         'cases main': {
+            'simple_transformation': simple_transformation,
             'cases_diagnosis_diagnosis_transformation': cases_diagnosis_diagnosis_transformation,
             'cases_diagnosis_ncs_criteria_transformation': cases_diagnosis_ncs_criteria_transformation,
             'cases_diagnosis_emg_criteria_transformation': cases_diagnosis_emg_criteria_transformation,
@@ -135,6 +115,44 @@ def main():
             'cases_differential_criteria_transformation': cases_differential_criteria_transformation,
         }
     }
+
+
+def simple_transformation(isDelete,
+                          file_with_name,
+                          column_to_transform,
+                          column_with_name,
+                          primary_key):
+    # Read in the file_with_name file
+    with_name_df = pd.read_excel(f'original/{file_with_name}')
+    # Read in the file_to_transform file
+    to_transform_df = pd.read_excel(
+        f'original/{file_transformation["file_to_transform"]}')
+
+    # Loop through the column transformations
+    for column_transformation in file_transformation['column_transformations']:
+        print(
+            f'Transforming {column_transformation["column_to_transform"]} in {file_transformation["file_to_transform"]}')
+        # If the column is to be deleted, delete it and continue to the next column transformation
+        if column_transformation['isDelete']:
+            print(
+                f'Deleting {column_transformation["column_to_transform"]}')
+            del to_transform_df[column_transformation['column_to_transform']]
+            continue
+
+        # Read in the file_with_name file
+        with_name_df = pd.read_excel(
+            f'original/{column_transformation["file_with_name"]}')
+
+        # Create a dictionary to map ID to nerve/muscle names
+        name_dict = with_name_df.set_index(
+            column_transformation['primary_key'])[column_transformation['column_with_name']].to_dict()
+
+        # Use the map function to replace the ID in the file_to_transform file with the corresponding nerve/muscle name
+        to_transform_df[column_transformation['column_to_transform']
+                        ] = to_transform_df[column_transformation['column_to_transform']].map(name_dict)
+    # Save the modified file_to_transformed file
+    to_transform_df.to_excel(
+        f'transformed/{file_transformation["export_file_name"]}', index=False)
 
 
 def cases_diagnosis_diagnosis_transformation():
@@ -216,8 +234,14 @@ def to_csv(file_name, df):
 
 
 if __name__ == '__main__':
-    # cases_main_cc_transformation(
-    #     'cases main transformed.xlsx', 'cases main.xlsx', 'cc')
+
+    # get from command line argument the desired output type
+    output_type = sys.argv[1]
+
+    # if output type is not csv or xlsx, exit
+    if output_type != 'csv' and output_type != 'xlsx':
+        print('Invalid output type. Must only be "csv" or "xlsx"')
+        exit()
 
     cases_main_cc_transformation(
         "cases main.xlsx", "cc relations.xlsx", "cc names.xlsx", "cases main transformed.xlsx")
